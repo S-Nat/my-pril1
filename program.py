13e5bd92bbec161c9231ee3f7653f563d09fd27a
S-Nat <45121361+S-Nat@users.noreply.github.com>
2021-03-20 17:22:53 +0300

    Delete program.py

--- program.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print('My first git program')
-    print('И я изменяю это каждый день')
-    print('Again')
-    print('UFO came and added this line')
-
-
-if __name__ == '__main__':
-    main()
